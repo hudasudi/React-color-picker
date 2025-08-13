3cd35411e75e9868583f2b6c1d6eba94dea1a3b6
hudas <huda.abdi-dahir-sudi.2023@uni.strath.ac.uk>
2025-08-13 21:59:29 +0100

    styling left

--- myapp/src/ColorPicker.jsx
+++ myapp/src/ColorPicker.jsx
@@ -2,6 +2,17 @@
 import React,{useState} from "react"
 function ColorPicker(){
 
+const [color,setColor] = useState("#ffffff");
+
+function handleColorChange(event){
+  setColor(event.target.value);
+}
+return(<div>
+    <h1>Color Picker</h1>
+    <p>Selected Color: {color}</p>
+    <label>Select Color: </label>
+    <input type="color" value={color} onChange={handleColorChange}></input>
+</div>);
 
 }
 

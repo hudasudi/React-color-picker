0ad6ca904459c026623c9598f96f15bf9e579060
hudas <huda.abdi-dahir-sudi.2023@uni.strath.ac.uk>
2025-08-16 16:29:43 +0100

    added css

--- myapp/src/ColorPicker.jsx
+++ myapp/src/ColorPicker.jsx
@@ -7,11 +7,13 @@ const [color,setColor] = useState("#ffffff");
 function handleColorChange(event){
   setColor(event.target.value);
 }
-return(<div>
+return(<div className="color-container">
     <h1>Color Picker</h1>
+     <div className="display-color" style={{backgroundColor: color}}>
     <p>Selected Color: {color}</p>
+    </div>
     <label>Select Color: </label>
-    <input type="color" value={color} onChange={handleColorChange}></input>
+    <input type="color" value={color} onChange={handleColorChange}></input>   
 </div>);
 
 }
